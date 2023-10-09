4ebcffb1324fdcb4513c36c217d58dd528cb0022
Shivam77607 <shivamsharma77607@gmail.com>
2023-10-09 21:51:30 +0530

    fix: add chakra provider

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -4,11 +4,11 @@ import { AttachmentIcon } from "@chakra-ui/icons";
 
 function ChatBox() {
     return (
-        <Box display="flex" alignItems="center" justifyContent="space-between" bgColor="white" px={16} py={8} pos="fixed" bottom={6} w="100%" maxW={375}>
-            <Input placeholder="Repy to @Rahul Yadav" variant="unstyled" border="none" p={5} w="80%" outline="none" fontSize={14} />
-            <Box display="flex" alignItems="center" justifyContent="space-between" gap={18}>
-                <Icon as={AttachmentIcon} w={20} h={20} color="#141E0D" bgColor="transparent" />
-                <Image src="/icons/send-icon.svg" w={20} h={20} color="#141E0D" bgColor="transparent" />
+        <Box display="flex" alignItems="center" justifyContent="space-between" bgColor="white" px={4} py={2} pos="fixed" bottom={2} w="100%" maxW={375}>
+            <Input placeholder="Repy to @Rahul Yadav" variant="unstyled" border="none" p={2} w="80%" outline="none" fontSize={14} />
+            <Box display="flex" alignItems="center" justifyContent="space-between" gap={8}>
+                <Icon as={AttachmentIcon} w="20px" h="20px" color="#141E0D" bgColor="transparent" />
+                <Image src="/icons/send-icon.svg" w="20px" h="20px" color="#141E0D" bgColor="transparent" />
             </Box>
         </Box >
     )
--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -8,27 +8,27 @@ function ChatHeader() {
     const [isEditing, setIsEditing] = useState(false)
 
     return (
-        <Box display={"flex"} flexDir="column" padding={14} pb={16} pt={50} gap={14} borderBottom="2px solid #E5E5E0">
+        <Box display={"flex"} flexDir="column" padding="14px" pb="16px" pt="50px" gap="14px" borderBottom="2px solid #E5E5E0">
             <Box display={"flex"} alignItems={"center"} justifyContent={"space-between"} w="100%">
-                <Box display="flex" alignItems="center" gap={10}>
-                    <ArrowBackIcon w={24} h={24} cursor="pointer" />
-                    <Input value={inputValue} fontSize={24} variant="unstyled" border={"none"} w={200} bgColor="transparent" outline="none" p={5} fontWeight={700} onChange={(e) => setInputValue(e.target.value)} isDisabled={!isEditing && true} />
+                <Box display="flex" alignItems="center" gap={3}>
+                    <ArrowBackIcon w="24px" h="24px" cursor="pointer" />
+                    <Input value={inputValue} fontSize="24px" variant="unstyled" border={"none"} w={200} bgColor="transparent" outline="none" p="5px" fontWeight={700} onChange={(e) => setInputValue(e.target.value)} isDisabled={!isEditing && true} />
                 </Box>
                 <Box onClick={() => setIsEditing(!isEditing)} cursor="pointer">
                     {isEditing ?
-                        <CheckIcon w={20} h={20} /> : <EditIcon w={20} h={20} />}
+                        <CheckIcon w="20px" h="20px" /> : <EditIcon w="20px" h="20px" />}
                 </Box>
             </Box>
             <Box display="flex" justifyContent="space-between" alignItems="center">
-                <Box display="flex" gap={10}>
+                <Box display="flex" gap={4}>
                     <Image src='images/group-pic.svg' alt="Group pic" borderRadius='full'
-                        boxSize={48} border="none" />
+                        boxSize="48px" border="none" />
                     <Box display="flex" flexDirection="column" justifyContent="space-between">
-                        <Heading as="h4" fontSize={18}><span style={{ color: "#606060", fontWeight: 600 }}>From </span>IGI Airport, T3</Heading>
-                        <Heading as="h4" fontSize={18}><span style={{ color: "#606060", fontWeight: 600 }}>To </span>Sector 28</Heading>
+                        <Heading as="h4" fontSize="18px"><span style={{ color: "#606060", fontWeight: 600 }}>From </span>IGI Airport, T3</Heading>
+                        <Heading as="h4" fontSize="18px"><span style={{ color: "#606060", fontWeight: 600 }}>To </span>Sector 28</Heading>
                     </Box>
                 </Box>
-                <Image src='icons/kebab-icon.svg' alt="kebab-icon" boxSize={24} cursor="pointer" />
+                <Image src='icons/kebab-icon.svg' alt="kebab-icon" boxSize="24px" cursor="pointer" />
             </Box>
         </Box >
     )
