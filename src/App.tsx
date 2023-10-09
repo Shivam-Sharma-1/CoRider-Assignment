619637a0d9f9ec65176849639494b938ef8feddd
Shivam77607 <shivamsharma77607@gmail.com>
2023-10-09 20:56:48 +0530

    feat: add ChatHeader

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
+import { Box } from "@chakra-ui/react"
+import ChatHeader from './components/ChatHeader';
 
 function App() {
     return (
-        <div >
-            <h1>Hello World</h1>
-        </div>
+        <Box w="100%" h="100%" maxW={375} minH="100svh" mx="auto" bgColor="#FAF9F4"><ChatHeader /></Box>
     );
 }
 
---
+++ src/components/ChatHeader.tsx
@@ -0,0 +1,39 @@
+import { EditablePreview, Box, useColorModeValue, IconButton, Input, useDisclosure, useEditableControls, ButtonGroup, SlideFade, Editable, Tooltip, EditableInput, Flex, Image, Heading, border } from "@chakra-ui/react";
+import { CheckIcon, ArrowBackIcon, CloseIcon, EditIcon } from "@chakra-ui/icons";
+import { useRef, useState } from "react";
+import KebabIcon from "./../../public/icons/kebab-icon.svg"
+
+
+function ChatHeader() {
+    const [inputValue, setInputValue] = useState("Trip 1")
+    const [isEditing, setIsEditing] = useState(false)
+
+    return (
+        <Box display={"flex"} flexDir="column" padding={14} pb={16} pt={50} gap={14} borderBottom="2px solid #E5E5E0">
+            <Box display={"flex"} alignItems={"center"} justifyContent={"space-between"} w="100%">
+                <Box display="flex" alignItems="center" gap={10}>
+                    <ArrowBackIcon w={24} h={24} cursor="pointer" />
+                    <Input value={inputValue} fontSize={24} variant="unstyled" border={"none"} w={200} bgColor="transparent" p={5} fontWeight={700} onChange={(e) => setInputValue(e.target.value)} isDisabled={!isEditing && true} />
+                </Box>
+                <Box onClick={() => setIsEditing(!isEditing)} cursor="pointer">
+                    {isEditing ?
+                        <CheckIcon w={20} h={20} /> : <EditIcon w={20} h={20} />}
+                </Box>
+            </Box>
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Box display="flex" gap={10}>
+                    <Image src='images/group-pic.svg' alt="Group pic" borderRadius='full'
+                        boxSize={48} border="none" />
+                    <Box display="flex" flexDirection="column" justifyContent="space-between">
+                        <Heading as="h4" fontSize={18}><span style={{ color: "#606060", fontWeight: 600 }}>From </span>IGI Airport, T3</Heading>
+                        <Heading as="h4" fontSize={18}><span style={{ color: "#606060", fontWeight: 600 }}>To </span>Sector 28</Heading>
+                    </Box>
+                </Box>
+                <Image src='icons/kebab-icon.svg' alt="kebab-icon" boxSize={24} cursor="pointer" />
+            </Box>
+        </Box >
+    )
+
+}
+
+export default ChatHeader;
