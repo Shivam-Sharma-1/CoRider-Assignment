adf198aec1c20558e05a291e12e4c232dd36fb96
Shivam77607 <shivamsharma77607@gmail.com>
2023-10-10 23:26:30 +0530

    fix: input disabled text color

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Box, Input, Image, Heading, Popover, PopoverTrigger, Portal, PopoverContent, PopoverBody, Text } from "@chakra-ui/react";
+import { Box, Input, Image, Heading, Popover, PopoverTrigger, Portal, PopoverContent, PopoverBody, Text, Avatar } from "@chakra-ui/react";
 import { CheckIcon, ArrowBackIcon, EditIcon } from "@chakra-ui/icons";
 
 const buttonData = [
@@ -32,7 +32,7 @@ function ChatHeader() {
             <Box display={"flex"} alignItems={"center"} justifyContent={"space-between"} w="100%">
                 <Box display="flex" alignItems="center" gap={3}>
                     <ArrowBackIcon w="24px" h="24px" cursor="pointer" />
-                    <Input value={inputValue} fontSize="24px" variant="unstyled" border={"none"} w={200} bgColor="transparent" outline="none" p="5px" fontWeight={700} onChange={(e) => setInputValue(e.target.value)} isDisabled={!isEditing && true} />
+                    <Input value={inputValue} fontSize="24px" variant="unstyled" border={"none"} w={200} bgColor="transparent" outline="none" p="5px" fontWeight={700} onChange={(e) => setInputValue(e.target.value)} isDisabled={!isEditing && true} _disabled={{ color: "initial" }} />
                 </Box>
                 <Box onClick={() => setIsEditing(!isEditing)} cursor="pointer">
                     {isEditing ?
@@ -41,8 +41,7 @@ function ChatHeader() {
             </Box>
             <Box display="flex" justifyContent="space-between" alignItems="center">
                 <Box display="flex" gap={4}>
-                    <Image src='images/group-pic.svg' alt="Group pic" borderRadius='full'
-                        boxSize="48px" border="none" />
+                    <Avatar src='images/group-pic.svg' name="Group pic" borderRadius="full" boxSize="48px" border="none" />
                     <Box display="flex" flexDirection="column" justifyContent="space-between">
                         <Heading as="h4" fontSize="18px"><span style={{ color: "#606060", fontWeight: 600 }}>From </span>IGI Airport, T3</Heading>
                         <Heading as="h4" fontSize="18px"><span style={{ color: "#606060", fontWeight: 600 }}>To </span>Sector 28</Heading>
