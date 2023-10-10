c1d79ff28ce3116244ffb0964d3afa187553f103
Shivam77607 <shivamsharma77607@gmail.com>
2023-10-10 23:17:38 +0530

    style: adjust style

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -22,12 +22,12 @@ const buttonData = [
 
 function ChatBox() {
     return (
-        <Box display="flex" alignItems="center" justifyContent="space-between" bgColor="white" px={4} py={2} w="100%" maxW={375} pos="fixed" bottom={2}>
+        <Box display="flex" alignItems="center" justifyContent="space-between" bgColor="white" px={5} py={2} w="100%" maxW={375} pos="fixed" bottom={2}>
             <Input placeholder="Reply to @Rahul Yadav" variant="unstyled" border="none" p={2} w="80%" outline="none" fontSize={14} />
-            <Box display="flex" alignItems="center" justifyContent="space-between" gap={8}>
+            <Box display="flex" alignItems="center" justifyContent="space-between" gap="16px">
                 <Popover placement="top" >
                     <Portal>
-                        <PopoverContent w="124px" h="44px" borderRadius="1000px" boxShadow="0px -1px 8px 0px rgba(0, 0, 0, 0.05)">
+                        <PopoverContent w="124px" h="44px" borderRadius="1000px" boxShadow="0px -1px 8px 0px rgba(0, 0, 0, 0.05)" outline="none">
                             <PopoverBody w="100%" h="100%" borderRadius="inherit" bgColor="rgba(0, 128, 0, 1)" boxShadow="0px -1px 8px 0px rgba(0, 0, 0, 0.05)" display="flex" gap="16px" alignItems="center" justifyContent="center">
 
                                 {buttonData.map((item) => (
